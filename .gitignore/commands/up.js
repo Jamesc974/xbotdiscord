87424f97db672831df6c29a25e3be736e7fe90dc
Jamesc974 <36702319+Jamesc974@users.noreply.github.com>
2018-03-28 16:11:44 +0200

    Update up.js

--- .gitignore/commands/up.js
+++ .gitignore/commands/up.js
@@ -2,6 +2,6 @@ const moment = require("moment")
 require("moment-duration-format")
 
 exports.run = (client, message, args) => {
-    const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+    const duration = moment.duration(client.uptime).format(" D [Jours], H [Heures], m [Minutes], s [Secondes]");
     message.channel.send(duration)
 }
