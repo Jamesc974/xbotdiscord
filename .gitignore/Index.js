b88c26f4c6901832e61b2dfe8cea275e41a910c8
Jamesc974 <36702319+Jamesc974@users.noreply.github.com>
2018-03-29 10:12:06 +0200

    Update Index.js

--- .gitignore/Index.js
+++ .gitignore/Index.js
@@ -9,7 +9,7 @@ var prefix = (process.env.PREFIX)
 
 client.on("ready", async () => {
     console.log(`${client.user.username} est en ligne sur ${client.guilds.size} serveurs!`);
-    client.user.setPresence({ game: { name: `[<nsfw] || connecté à ${client.guilds.size} serveur || créé par TarKyo`}})
+    client.user.setPresence({ game: { name: `[xnsfw] || connecté à ${client.guilds.size} serveur || créé par TarKyo`}})
   
   });
 
