1808ce5605e34ec5fdad84747e16e7b2fda03f44
Jamesc974 <36702319+Jamesc974@users.noreply.github.com>
2018-04-10 13:17:12 +0200

    Create duel.js

---
+++ .gitignore/commands/duel.js
@@ -0,0 +1,63 @@
+const Discord = require('discord.js');
+
+
+
+exports.run = async (client, msg) => {
+
+  const duelhelp = new Discord.MessageEmbed()
+
+    .setTitle('Duel Usage')
+
+    .setAuthor('Yuga')
+
+    .setColor(0x32CD32)
+
+    .addField('About', 'Fight with another user!', false)
+
+    .addField('Usage', 'y!duel <user mention>', false)
+
+    .addField('Perms required', 'None')
+
+    .setThumbnail(client.user.avatarURL())
+
+    .setTimestamp();
+
+  const user1 = msg.author;
+
+  const user2 = msg.mentions.users.first();
+
+  if (!user2) {
+
+    return msg.channel.send({
+
+      embed: duelhelp,
+
+    });
+
+  }
+
+
+
+  if (user1 == user2) return msg.reply('You can\'t duel yourself!');
+
+  const users = [];
+
+  await users.push(user1);
+
+  await users.push(user2);
+
+  const winner = users[Math.floor(Math.random() * users.length)];
+
+  const message = await msg.channel.send(`${user1} is dueling ${user2}!`);
+
+  await message.edit('⚔ Dueling');
+
+  await message.edit('⚔ Dueling.');
+
+  await message.edit('⚔ Dueling..');
+
+  await message.edit('⚔ Dueling...');
+
+  await message.edit(`${winner} has won!`);
+
+};
