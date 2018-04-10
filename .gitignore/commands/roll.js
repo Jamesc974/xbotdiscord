04b70d42906cd7b7a2c8e7bff584f84990796cac
Jamesc974 <36702319+Jamesc974@users.noreply.github.com>
2018-04-10 13:07:44 +0200

    Create roll.js

---
+++ .gitignore/commands/roll.js
@@ -0,0 +1,24 @@
+function randomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+  
+ exports.run = async (client, msg) => {
+ 
+      const slotOptions = ['💲', '🌮', '🍇', '🍎', '🍅', '🍓', '🍉', '🍋', '🍪'];
+      const slot1 = slotOptions[randomInt(0, 8)];
+      JSON.stringify(slot1);
+      const slot2 = slotOptions[randomInt(0, 8)];
+      JSON.stringify(slot2);
+      const slot3 = slotOptions[randomInt(0, 8)];
+      JSON.stringify(slot3);
+      const slotMessage = await msg.channel.send(`**${msg.author.username}** a Lancé la loterie !💸 `);
+      slotMessage.edit(`**${msg.author.username}** a Lancé la loterie !💸 \n\n | |`);
+      slotMessage.edit(`**${msg.author.username}** a Lancé la loterie !💸 \n\n${slot1}| |`);
+      slotMessage.edit(`**${msg.author.username}** a Lancé la loterie !💸 \n\n${slot1} | ${slot2} |`);
+      slotMessage.edit(`**${msg.author.username}** a Lancé la loterie !💸 \n\n${slot1} | ${slot2} | ${slot3}`);
+      if (slot1 == slot2 && slot1 == slot3 && slot2 == slot3) {
+        slotMessage.edit(`**${msg.author.username}** a Lancé la loterie !💸 \n\n${slot1} | ${slot2} | ${slot3}\n\nTu as gagné bien jouer!🌟🎉`);
+      } else {
+        slotMessage.edit(`**${msg.author.username}** a Lancé la loterie !💸 \n\n${slot1} | ${slot2} | ${slot3}\n\nTu as perdu ! 😢 \nEssaye plus tard 🌗`);
+      }
+};
