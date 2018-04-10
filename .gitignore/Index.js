5feb358138d8797b1a2c39e69813823bc3b5ea4e
Jamesc974 <36702319+Jamesc974@users.noreply.github.com>
2018-04-10 12:28:54 +0200

    Update Index.js

--- .gitignore/Index.js
+++ .gitignore/Index.js
@@ -96,21 +96,21 @@ client.on('message', message => {
     }
 });
 
-client.on(client, msg) => {
-  if (msg.content === prefix + "avatar") {
-      const user = msg.mentions.users.first();
-      if(!user) return msg.channel.send("❓Tu as mis son nom").then(msg => msg.delete(5000));
-      if (!user.avatarURL) return msg.channel.send("Il n'a pas d'avatar").then(msg => msg.delete(5000));
-      msg.delete();
-      var iconm = msg.author.avatarURL
-      var embed = new Discord.RichEmbed()
-      .setAuthor(`Avatar`, iconm)
-      .setImage(user.avatarURL)
-      .setColor("#DBA901")
-      .setTimestamp()
-      .setFooter(`Demandé par ${msg.author.tag}`, iconm);
-      msg.channel.sendMessage(embed)
-  }
-};
+client.on('msg', msg => {
+    if (msg.content === prefix + "avatar") {
+        const user = msg.mentions.users.first();
+        if(!user) return msg.channel.send("❓Tu as mis son nom").then(msg => msg.delete(5000));
+        if (!user.avatarURL) return msg.channel.send("Il n'a pas d'avatar").then(msg => msg.delete(5000));
+        msg.delete();
+        var iconm = msg.author.avatarURL
+        var embed = new Discord.RichEmbed()
+        .setAuthor(`Avatar`, iconm)
+        .setImage(user.avatarURL)
+        .setColor("#DBA901")
+        .setTimestamp()
+        .setFooter(`Demandé par ${msg.author.tag}`, iconm);
+        msg.channel.sendMessage(embed)
+    }
+  });
 
 client.login(token);
